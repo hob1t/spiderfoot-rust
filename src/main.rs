02e26d950d0ade7caca3a859383aa9e813c7192f
h0b1t <h0b1t202202@gmail.com>
2026-02-06 20:11:14 +0200

    SFR-3 Added trait, SpiderfootModule

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+mod core;
+
+use core::Target;
+
 fn main() {
-    println!("Hello, world!");
+    let domain = Target::Domain("example.com".to_string());
+    let ip     = Target::IpAddr("8.8.8.8".to_string());
+
+    println!("Target 1: {} → {}", domain.kind(), domain.value());
+    println!("Target 2: {} → {}", ip.kind(),     ip.value());
 }
